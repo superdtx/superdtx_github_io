28c0e632ac7ce9eeb6df70aff6c034753067d585
Tanxin Du <xin24914@gmail.com>
2017-07-15 22:58:12 -0400

    Revert "change the way to use the babel loader in config"
    This reverts commit 9641227e3836e169bf687db0b60497b89e3c6f9f.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -165,18 +165,14 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-            options: {
-              
-              // This is a feature of `babel-loader` for webpack (not Babel itself).
-              // It enables caching results in ./node_modules/.cache/babel-loader/
-              // directory for faster rebuilds.
-              cacheDirectory: true,
-            }
-          }
-        ],
+        loader: require.resolve('babel-loader'),
+        options: {
+          
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+        },
       },
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
