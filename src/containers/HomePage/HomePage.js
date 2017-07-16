66de2f4f055e57e92b7dd41a3b3a33607ddca2ab
Tanxin Du <xin24914@gmail.com>
2017-07-15 23:01:27 -0400

    test react-hot-loader 3.0

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -3,9 +3,25 @@ import { Container, Video } from './HomePage.style';
 
 export default class HomePage extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: 0
+		};
+	}
+
+	increment(){
+		this.setState({
+			data: this.state.data + 1
+		});
+	}
+
 	render() {
 		return (
 			<Container>
+				<h1>Hello World!!</h1>
+				<p>{this.state.data}</p>
+				<button onClick={this.increment.bind(this)}>Increment</button>
 				<Video autoPlay loop>
 					<source src={require('../../assets/zozi_homepage_720p_v13.webm')} type="video/webm"/>
 					<source src={require('../../assets/zozi_homepage_720p_v13.mp4')} type="video/mp4"/>
